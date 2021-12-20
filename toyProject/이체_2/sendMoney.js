7ce388a8363cb5ad5ebd273f77c24287ee5f800c
Sweet-salt <79562182+Sweet-salt@users.noreply.github.com>
2021-12-20 22:23:13 +0900

    feat: 그래프 데이터 찍어내는 건 완료
    데이터 정리가 필요함 일단 지출부분을 불러오는 거기 때문에 수입부분을 필터로 지워야함 그리고 2틀단위로 짝수로 묶어서 막대 그래프로 출력해야함.

---
+++ toyProject/이체_2/sendMoney.js
@@ -0,0 +1,13 @@
+
+function openBox() {
+    const showNext = document.querySelector(".number__line")
+    
+    if(showNext !== undefined){
+
+        const next = document.querySelector('.next');
+        next.style.width = "377px";
+        next.style.height = "48px";
+        next.style.background = "#ff5f00";
+    }
+}
+
---
+++ toyProject/지출관리/graph.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById('today__report').getContext('2d');
+var url = "https://jeondoh.github.io/FC_Lecture/ToyProject01/files/A1jo.json";
+
+
+function draw4(){
+    
+    $.getJSON('https://jeondoh.github.io/FC_Lecture/ToyProject01/files/A1jo.json', function (json_data) {
+        console.log(json_data)
+        const barChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["02", "04", "06", "08", "10", "12", "14",
+                         "16", "18", "20", "22"],
+                datasets: [
+                    {
+                        label: "일간 리포트",
+                        data: [
+                            json_data.bankList[0].price,
+                            json_data.bankList[1].price,
+                            json_data.bankList[2].price,
+                            json_data.bankList[3].price
+                        ]
+                    }
+                ]
+            }
+        })
+    })
+}
+draw4();
+
+
+const getdata = getData()
+console.log(getdata)
+
+
+async function getData() {
+    const api = url
+    const response = fetch(api).then(res => res.json()).then(json => console.log(json.bankList));
+    return response
+}
